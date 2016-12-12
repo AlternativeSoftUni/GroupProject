b905722dad72a85ebfe38beab25ede1f8d148123
StoyanTenev <stoian88lz@gmail.com>
2016-12-12 21:43:29 +0200

    Create new Entity
    Create new entity for upload picture .

---
+++ src/main/java/softuniBlog/entity/Picture.java
@@ -0,0 +1,51 @@
+package softuniBlog.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "pictures")
+public class Picture {
+
+    private Integer id;
+    private byte[] picture;
+    private Article article;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Lob
+    @Column(name = "pictures", nullable = false, columnDefinition = "mediumblob")
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public Picture() {
+    }
+
+    public Picture(byte[] picture, Article article) {
+        this.picture = picture;
+        this.article = article;
+    }
+}
